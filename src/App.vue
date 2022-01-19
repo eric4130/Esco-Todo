5e8e571cc77a585b04f31ca6a3e2f0001559e1ca
eric4130 <synthpad413@gmail.com>
2022-01-19 10:27:27 -0500

    fixed images

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,68 @@
 <template>
-<v-app>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      color="grey lighten-1"
-    >
-    <v-col class="text-center mb-n6">
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app color="grey lighten-1">
+      <v-col class="text-center mb-n6">
         <v-avatar color="blue darken-2" size="80">
-              <span class="white--text text-h4">ES</span>
+          <img :src="require('./assets/profile.jpg')" alt="John" />
         </v-avatar>
         <p class="white--text mt-1 text-h6">Eric</p>
-    </v-col>
-    <v-list>
-        <v-list-item v-for="(link, i) in links" :key="i" router :to="link.route">
+      </v-col>
+      <v-list>
+        <v-list-item
+          v-for="(link, i) in links"
+          :key="i"
+          router
+          :to="link.route"
+        >
           <v-list-item-action>
             <v-icon color="black">{{ link.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content right>
-            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+            <v-list-item-title class="white--text">{{
+              link.text
+            }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-    </v-list>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app color="grey lighten-2">
-      <v-app-bar-nav-icon class="black--text"  @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title class="text-uppercase grey--text text--darken-3 px-0">
-            <span class="font-weight-light">ESCO</span>
-            <span class="font-weight-bold">Todo</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn text class="mr-2  black--text">
-            <span>Sign Out</span>
-            <v-icon large right>mdi-arrow-right-bold-box-outline</v-icon>
-        </v-btn>
+      <v-app-bar-nav-icon
+        class="black--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text text--darken-3 px-0">
+        <span class="font-weight-light">ESCO</span>
+        <span class="font-weight-bold">Todo</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text class="mr-2 black--text">
+        <span>Sign Out</span>
+        <v-icon large right>mdi-arrow-right-bold-box-outline</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main class="grey lighten-1">
-      <router-view/>
+      <router-view />
     </v-main>
-
-</v-app>
+  </v-app>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        drawer:null,
-        links: [
-          {icon:'mdi-dock-window',text:'Dashboard', route: '/'},
-          {icon:'mdi-account-details',text:'My Projects', route: '/projects'},
-          {icon:'mdi-account-group',text:'Team', route: '/team'},
-        ]
-      }
-    }
-   
-  }
+export default {
+  data() {
+    return {
+      drawer: null,
+      links: [
+        { icon: "mdi-dock-window", text: "Dashboard", route: "/" },
+        {
+          icon: "mdi-account-details",
+          text: "My Projects",
+          route: "/projects",
+        },
+        { icon: "mdi-account-group", text: "Team", route: "/team" },
+      ],
+    };
+  },
+};
 </script>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,53 +1,63 @@
 <template>
   <div class="team">
+    <h1 class="white--text ma-8 header">Team</h1>
 
-        <h1 class="white--text ma-8 header">Team</h1>
-        
-        <v-container class="my-5">
-              <v-row>
-                  <v-col cols="12" sm="6" md="4" lg="3" v-for="(member, i) in team" :key="i">
-                        <v-card class="text-center">
-                              <v-avatar color="blue darken-2" class="mt-3" size="80">
-                                    <span class="white--text text-h4">{{ member.avatar }}</span>
-                              </v-avatar>
-                              <v-card-text class="pb-2">
-                                  <div class="text-h4">{{ member.name }} </div>
-                                  <div class="text-h5 grey--text">{{ member.role }} </div>
-                              </v-card-text>
-                              <v-card-actions class="justify-center">
-                                    <v-btn color="grey" class="mb-1"> 
-                                          <v-icon left>mdi-message</v-icon>
-                                          <span>Message</span>
-                                    </v-btn>
-                              </v-card-actions>
-                        </v-card>
-                  </v-col>
-
-              </v-row>
-        </v-container>
-    
-
-</div>
+    <v-container class="my-5">
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          v-for="(member, i) in team"
+          :key="i"
+        >
+          <v-card class="text-center">
+            <v-avatar class="mt-3" size="120">
+              <img :src="require(`../assets/${member.image}`)" alt="John" />
+            </v-avatar>
+            <v-card-text class="pb-2">
+              <div class="text-h4">{{ member.name }}</div>
+              <div class="text-h5 grey--text">{{ member.role }}</div>
+            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn color="grey" class="mb-1">
+                <v-icon left>mdi-message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-          return {
-                team: [
-                      {name:'Eric', role: 'Web Developer', avatar: 'ES'},
-                      {name:'Elizabeth', role: 'Being A Butt', avatar: 'ES'},
-                      {name:'Lauren', role: 'Ultimate Grump', avatar: 'LS'},
-                      {name:'Mom', role: 'Mom Stuff', avatar: 'KS'},
-                      {name:'Dad', role: 'Dad Stuff', avatar: 'KS'}                 
-                ]
-          }
-    }
-}
+  data() {
+    return {
+      team: [
+        { name: "Eric", role: "Web Developer", image: "profile.jpg" },
+        {
+          name: "Elizabeth",
+          role: "Being A Butt",
+          image: "logo.svg",
+        },
+        {
+          name: "Lauren",
+          role: "Ultimate Grump",
+          image: "profile.jpg",
+        },
+        { name: "Mom", role: "Mom Stuff", image: "logo.svg" },
+        { name: "Dad", role: "Dad Stuff", image: "profile.jpg" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
 .header {
   text-align: center;
 }
